ce1ce6a0530522cfac360d4ee194cb62b95fbb8a
Leopold <leopolddag@gmail.com>
2025-03-21 16:29:36 +0100

    Mise a jour du reste des fichiers

--- src/main/java/com/openclassrooms/safetyNet/exceptions/FirestationNotFoundException.java
+++ src/main/java/com/openclassrooms/safetyNet/exceptions/FirestationNotFoundException.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.safetyNet.exceptions;
 
-public class FirestationNotFoundException extends Exception{
-    public FirestationNotFoundException(String message) {super(message);}
+public class FirestationNotFoundException extends Exception {
+    public FirestationNotFoundException(String message) {
+        super(message);
+    }
 }
